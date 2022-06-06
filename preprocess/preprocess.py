c7bc71bb6cf919bc0e0bd228c3231191dc5deb84
Mikami Xiao <mikamixiao@Mikamis-MacBook-Pro.local>
2022-06-05 21:15:25 -0400

    add new preprocessing method

---
+++ preprocess/preprocess.py
@@ -0,0 +1,136 @@
+import numpy as np
+import glob
+import ants
+import nibabel as nib
+import os
+import argparse
+
+
+def parse_command_line():
+    parser = argparse.ArgumentParser(
+        description='pipeline for data preprocessing')
+    parser.add_argument('-bp', metavar='base path', type=str,
+                        help="Absolute path of the base directory")
+    parser.add_argument('-ip', metavar='image path', type=str,
+                        help="Relative path of the image directory")
+    parser.add_argument('-sp', metavar='segmentation path', type=str,
+                        help="Relative path of the image directory")
+    parser.add_argument('-op', metavar='preprocessing result output path', type=str, default='output',
+                        help='Relative path of the preprocessing result directory')
+    parser.add_argument('-rs', metavar='shape after resizing', type=int, nargs='+',
+                        help='shape after resizing the image and segmentation. Expected to be 2^N')
+    argv = parser.parse_args()
+    return argv
+
+
+def pad(array, shape):
+    """
+    Zero-pads an array to a given shape. Returns the padded array and crop slices.
+    """
+    if array.shape == tuple(shape):
+        return array, ...
+
+    padded = np.zeros(shape, dtype=array.dtype)
+    offsets = [int((p - v) / 2) for p, v in zip(shape, array.shape)]
+    slices = tuple([slice(offset, l + offset)
+                   for offset, l in zip(offsets, array.shape)])
+    padded[slices] = array
+
+    return padded
+
+
+def crop_and_flip(nib_img, nib_seg, ants_img, ants_seg):
+    img = nib_img.get_fdata()
+    seg = nib_seg.get_fdata()
+    gem = ants.label_geometry_measures(ants_seg, ants_img)
+    low_x = min(list(gem.loc[:, 'BoundingBoxLower_x']))
+    upp_x = max(list(gem.loc[:, 'BoundingBoxUpper_x']))
+    low_y = min(list(gem.loc[:, 'BoundingBoxLower_y']))
+    upp_y = max(list(gem.loc[:, 'BoundingBoxUpper_y']))
+    low_z = min(list(gem.loc[:, 'BoundingBoxLower_z']))
+    upp_z = max(list(gem.loc[:, 'BoundingBoxUpper_z']))
+
+    # Compute mid point
+    mid_x = int((low_x + upp_x) / 2)
+
+    left_img = img[low_x:mid_x, low_y:upp_y, low_z:upp_z]
+    left_seg = seg[low_x:mid_x, low_y:upp_y, low_z:upp_z]
+
+    flipped_right_img = np.flip(
+        img[mid_x:upp_x, low_y:upp_y, low_z:upp_z], axis=0)
+    flipped_right_seg = np.flip(
+        seg[mid_x:upp_x, low_y:upp_y, low_z:upp_z], axis=0)
+    return left_img, left_seg, flipped_right_img, flipped_right_seg
+
+
+def load_data(img_path, seg_path):
+    nib_seg = nib.load(seg_path)
+    nib_img = nib.load(img_path)
+    ants_seg = ants.image_read(seg_path)
+    ants_img = ants.image_read(img_path)
+    return nib_img, nib_seg, ants_img, ants_seg
+
+
+def save_file(left_img, left_seg, flipped_right_img, flipped_right_seg, nib_img, nib_seg, output_img, output_seg, scan_id):
+    left_img_nii = nib.Nifti1Image(
+        left_img, affine=nib_img.affine)
+    left_seg_nii = nib.Nifti1Image(
+        left_seg, affine=nib_seg.affine)
+    right_img_nii = nib.Nifti1Image(
+        flipped_right_img, affine=nib_img.affine)
+    right_seg_nii = nib.Nifti1Image(
+        flipped_right_seg, affine=nib_seg.affine)
+    left_img_nii.to_filename(os.path.join(
+        output_img, 'left_' + scan_id + '.nii.gz'))
+    left_seg_nii.to_filename(os.path.join(
+        output_seg, 'left_' + scan_id + '.nii.gz'))
+    right_img_nii.to_filename(os.path.join(
+        output_img, 'right_' + scan_id + '.nii.gz'))
+    right_seg_nii.to_filename(os.path.join(
+        output_seg, 'right_' + scan_id + '.nii.gz'))
+
+
+def main():
+    args = parse_command_line()
+    base_path = args.bp
+    image_path = os.path.join(base_path, args.ip)
+    seg_path = os.path.join(base_path, args.sp)
+    output_path = os.path.join(base_path, args.op)
+    resize_shape = args.rs
+
+    output_img = os.path.join(output_path, 'images')
+    output_seg = os.path.join(output_path, 'labels')
+
+    try:
+        os.mkdir(output_path)
+    except:
+        print(f'{output_path} is already existed')
+
+    try:
+        os.mkdir(output_img)
+    except:
+        print(f'{output_img} is already existed')
+
+    try:
+        os.mkdir(output_seg)
+    except:
+        print(f'{output_seg} is already existed')
+
+    for i in sorted(glob.glob(image_path + '/*.nii.gz')):
+        id = os.path.basename(i).split('.')[0]
+        label_path = os.path.join(seg_path, id + '.nii.gz')
+        nib_img, nib_seg, ants_img, ants_seg = load_data(i, label_path)
+        left_img, left_seg, flipped_right_img, flipped_right_seg = crop_and_flip(
+            nib_img, nib_seg, ants_img, ants_seg)
+        print(
+            'Scan ID: ' + id + f', before cropping: {nib_img.get_fdata().shape}, after cropping: {left_img.shape} and {flipped_right_img.shape}, padding to {tuple(resize_shape)}')
+        left_img = pad(left_img, resize_shape)
+        left_seg = pad(left_seg, resize_shape)
+        flipped_right_img = pad(flipped_right_img, resize_shape)
+        flipped_right_seg = pad(flipped_right_seg, resize_shape)
+        save_file(left_img, left_seg, flipped_right_img, flipped_right_seg,
+                  nib_img, nib_seg, output_img, output_seg, id)
+
+
+if __name__ == '__main__':
+    main()
--- scripts/deep_atlas.py
+++ scripts/deep_atlas.py
@@ -1,3 +1,12 @@
+from train import (
+    train_network
+)
+from network import (
+    regNet, segNet
+)
+from process_data import (
+    split_data, load_seg_dataset, load_reg_dataset, take_data_pairs, subdivide_list_of_data_pairs
+)
 import monai
 import torch
 import itk
@@ -12,36 +21,28 @@ sys.path.insert(0, '/home/ameen/DeepAtlas/preprocess')
 sys.path.insert(0, '/home/ameen/DeepAtlas/network')
 sys.path.insert(0, '/home/ameen/DeepAtlas/train')
 
-from process_data import (
-    split_data, load_seg_dataset, load_reg_dataset, take_data_pairs, subdivide_list_of_data_pairs
-)
-from network import (
-    regNet, segNet
-)
-from train import (
-    train_network
-)
+
 def parse_command_line():
     print('---'*10)
     print('Parsing Command Line Arguments')
     parser = argparse.ArgumentParser(
-        description='pipeline for dataset co-alignment')
+        description='pipeline for deep atlas')
     parser.add_argument('-bp', metavar='base path', type=str,
                         help="Absolute path of the base directory")
     parser.add_argument('-ip', metavar='image path', type=str,
                         help="Relative path of the image directory")
     parser.add_argument('-sp', metavar='segmentation path', type=str,
                         help="Relative path of the image directory")
-    parser.add_argument('-op', metavar='preprocessing result output path', type=str,default='output',
+    parser.add_argument('-op', metavar='preprocessing result output path', type=str, default='output',
                         help='Relative path of the preprocessing result directory')
     parser.add_argument('-ns', metavar='number of segmentations', type=int, default=3,
                         help='number of segmentations used for training')
     parser.add_argument('-sd', metavar='spatial dimension', type=int, default=3,
                         help='spatial dimension of dataset')
-    #parser.add_argument('-ch', metavar='sequence of channels', type=int, nargs='+',
-                        #help='sequence of channels. Top block first. The length of `channels` should be no less than 2.')
-    #parser.add_argument('-st', metavar='sequence of strides', type=int, nargs='+',
-                        #help='sequence of convolution strides. The length of `stride` should equal to `len(channels) - 1')
+    # parser.add_argument('-ch', metavar='sequence of channels', type=int, nargs='+',
+    # help='sequence of channels. Top block first. The length of `channels` should be no less than 2.')
+    # parser.add_argument('-st', metavar='sequence of strides', type=int, nargs='+',
+    # help='sequence of convolution strides. The length of `stride` should equal to `len(channels) - 1')
     parser.add_argument('-dr', metavar='value of dropout', type=float, default=0.0,
                         help='dropout ratio. Defaults to no dropout.')
     parser.add_argument('-gpu', metavar='id of gpu', type=str, default='0',
@@ -67,6 +68,7 @@ def parse_command_line():
     argv = parser.parse_args()
     return argv
 
+
 def main():
     args = parse_command_line()
     base_path = args.bp
@@ -77,15 +79,15 @@ def main():
     spatial_dim = args.sd
     dropout = args.dr
     activation_type = args.at
-    normalization_type = args.nm 
+    normalization_type = args.nm
     num_res = args.nr
     gpu = args.gpu
-    lr_reg = args.lr 
-    lr_seg = args.ls 
+    lr_reg = args.lr
+    lr_seg = args.ls
     lam_a = args.ba
     lam_sp = args.bs
     max_epoch = args.me
-    val_step = args.vs 
+    val_step = args.vs
     device = torch.device("cuda:" + gpu)
     try:
         os.mkdir(output_path)
@@ -103,22 +105,23 @@ def main():
     data_seg_unavailable = list(filter(lambda d: 'seg' not in d.keys(), train))
     data_seg_available_train, data_seg_available_valid = \
         monai.data.utils.partition_dataset(data_seg_available, ratios=(8, 2))
-    dataset_seg_available_train, dataset_seg_available_valid = load_seg_dataset(data_seg_available_train, data_seg_available_valid)
+    dataset_seg_available_train, dataset_seg_available_valid = load_seg_dataset(
+        data_seg_available_train, data_seg_available_valid)
     data_item = random.choice(dataset_seg_available_train)
     num_label = len(torch.unique(data_item['seg']))
     print('---'*10)
     print('prepare segmentation network')
     seg_net = segNet(
-                    spatial_dim=spatial_dim,  # spatial dims
-                    in_channel=1,  # input channels
-                    out_channel=num_label,  # output channels
-                    channel=(8, 16, 16, 32, 32, 64, 64),  # channel sequence
-                    stride=(1, 2, 1, 2, 1, 2),  # convolutional strides
-                    dropouts=dropout,
-                    acts=activation_type,
-                    norms=normalization_type,
-                    num_res_unit=num_res
-                    )
+        spatial_dim=spatial_dim,  # spatial dims
+        in_channel=1,  # input channels
+        out_channel=num_label,  # output channels
+        channel=(8, 16, 16, 32, 32, 64, 64),  # channel sequence
+        stride=(1, 2, 1, 2, 1, 2),  # convolutional strides
+        dropouts=dropout,
+        acts=activation_type,
+        norms=normalization_type,
+        num_res_unit=num_res
+    )
     print(seg_net)
     '''
     print('---'*10)
@@ -139,10 +142,12 @@ def main():
     )
     data_pairs_valid = take_data_pairs(data_valid)
     data_pairs_train = take_data_pairs(data_train)
-    data_pairs_valid_subdivided = subdivide_list_of_data_pairs(data_pairs_valid)
-    data_pairs_train_subdivided = subdivide_list_of_data_pairs(data_pairs_train)
-    #print(data_pairs_train_subdivided)
-    #print(data_pairs_valid_subdivided)
+    data_pairs_valid_subdivided = subdivide_list_of_data_pairs(
+        data_pairs_valid)
+    data_pairs_train_subdivided = subdivide_list_of_data_pairs(
+        data_pairs_train)
+    # print(data_pairs_train_subdivided)
+    # print(data_pairs_valid_subdivided)
     num_train_reg_net = len(data_pairs_train)
     num_valid_reg_net = len(data_pairs_valid)
     num_train_both = len(data_pairs_train_subdivided['01']) +\
@@ -153,20 +158,21 @@ def main():
     and an additional {num_train_reg_net - num_train_both} to train reg_net alone.""")
     print(f"We have {num_valid_reg_net} pairs for reg_net validation.")
 
-    dataset_pairs_train_subdivided, dataset_pairs_valid_subdivided = load_reg_dataset(data_pairs_train_subdivided, data_pairs_valid_subdivided)
+    dataset_pairs_train_subdivided, dataset_pairs_valid_subdivided = load_reg_dataset(
+        data_pairs_train_subdivided, data_pairs_valid_subdivided)
     print('---'*10)
     print('prepare registration network')
     reg_net = regNet(
-                    spatial_dim=spatial_dim,  # spatial dims
-                    in_channel=2,  # input channels
-                    out_channel=spatial_dim,  # output channels
-                    channel=(16, 32, 32, 32, 32),  # channel sequence
-                    stride=(1, 2, 2, 2),  # convolutional strides
-                    dropouts=dropout,
-                    acts=activation_type,
-                    norms=normalization_type,
-                    num_res_unit=num_res
-                    )
+        spatial_dim=spatial_dim,  # spatial dims
+        in_channel=2,  # input channels
+        out_channel=spatial_dim,  # output channels
+        channel=(16, 32, 32, 32, 32),  # channel sequence
+        stride=(1, 2, 2, 2),  # convolutional strides
+        dropouts=dropout,
+        acts=activation_type,
+        norms=normalization_type,
+        num_res_unit=num_res
+    )
     print(reg_net)
     datasets = list(dataset_pairs_train_subdivided.values())
     datasets_combined = sum(datasets[1:], datasets[0])
@@ -203,7 +209,8 @@ def main():
             num_workers=4,
             shuffle=True
         )
-        if len(dataset) > 0 else []  # empty dataloaders are not a thing-- put an empty list if needed
+        # empty dataloaders are not a thing-- put an empty list if needed
+        if len(dataset) > 0 else []
         for seg_availability, dataset in dataset_pairs_train_subdivided.items()
     }
     dataloader_valid_reg = {
@@ -213,25 +220,27 @@ def main():
             num_workers=4,
             shuffle=True  # Shuffle validation data because we will only take a sample for validation each time
         )
-        if len(dataset) > 0 else []  # empty dataloaders are not a thing-- put an empty list if needed
+        # empty dataloaders are not a thing-- put an empty list if needed
+        if len(dataset) > 0 else []
         for seg_availability, dataset in dataset_pairs_valid_subdivided.items()
     }
 
-    train_network(dataloader_train_reg, 
-          dataloader_valid_reg, 
-          dataloader_train_seg, 
-          dataloader_valid_seg, 
-          device,
-          seg_net,
-          reg_net,
-          image_scale,
-          num_label,
-          lr_reg,
-          lr_seg,
-          lam_a,
-          lam_sp,
-          max_epoch,
-          val_step)
+    train_network(dataloader_train_reg,
+                  dataloader_valid_reg,
+                  dataloader_train_seg,
+                  dataloader_valid_seg,
+                  device,
+                  seg_net,
+                  reg_net,
+                  image_scale,
+                  num_label,
+                  lr_reg,
+                  lr_seg,
+                  lam_a,
+                  lam_sp,
+                  max_epoch,
+                  val_step)
+
 
 if __name__ == '__main__':
     # Set deterministic training for reproducibility
